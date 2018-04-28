3a1a0f09c921c8690d9d598193690c17a7804376
Abhay Bhargav <abhay@we45.com>
2018-04-28 15:24:18 +0530

    small changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RoboWFuzz',
-    version='0.1',
+    version='1.0',
     packages=[''],
     package_dir={'': 'robowfuzz'},
     url='',
