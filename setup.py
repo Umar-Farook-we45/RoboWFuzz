25e6964c934d7d7f3564346947519ff41e6790f7
Abhay Bhargav <abhay@we45.com>
2018-05-02 09:37:40 +0530

    changed back to JSON

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='RoboWFuzz',
-    version='1.0',
+    version='1.1',
     packages=[''],
     package_dir={'': 'robowfuzz'},
     url='',
